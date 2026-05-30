80f62237405df376248a04053805fbd973b2daa1
Lautaro Flores <lautaroflores47@gmail.com>
2026-05-30 18:55:49 -0300

    fix(medical-certificate): aplica regla de unicidad en la modificacion

--- packages/api/src/domain/MedicalCertificateRepository.ts
+++ packages/api/src/domain/MedicalCertificateRepository.ts
@@ -5,5 +5,6 @@ export interface MedicalCertificateRepository {
     findAll(): Promise<MedicalCertificateDTO[]>;
     findById(id: string): Promise<MedicalCertificateDTO | null>;
     updateValidationStatus(id: string, isValidated: boolean): Promise<MedicalCertificateDTO>;
+    validateAndInvalidateOthers(id: string, memberId: string): Promise<MedicalCertificateDTO>;
     softDelete(id: string): Promise<void>;
 }
